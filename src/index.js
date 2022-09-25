f80496b9fd35fe58045351b2450084ea88b09ab7
Mikolaj <mikolaj35@wp.pl>
2022-09-25 18:38:49 +0200

    Heroku deploy rearrange

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ const Task = require("./models/task");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const cors = require("cors")
+const path = require("path")
 
 const app = express();
 const port = process.env.PORT;
+
+require("dotenv").config()
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 app.use(cors({credentials: true}));
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
